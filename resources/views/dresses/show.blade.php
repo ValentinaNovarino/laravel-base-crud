0bab9a6c71678e675b0ea031867d2b47be91cda1
ValentinaNovarino <valenovarino@gmail.com>
2021-01-21 15:58:21 +0100

    modificato layout pagina dettaglio

--- resources/views/dresses/show.blade.php
+++ resources/views/dresses/show.blade.php
@@ -4,15 +4,13 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <h1 class="text-center">Dettagli vestito con id: {{ $dress->id }}</h1>
-                <ul>
-                    <li>Nome: {{ $dress->type }}</li>
-                    <li>Marca: {{ $dress->brand }}</li>
-                    <li>Prezzo: {{ number_format($dress->price, 2, ',', ' ') }}</li>
-                    <li>Colore: {{ $dress->color }}</li>
-                    <li>Taglia: {{ $dress->size }}</li>
-                    <li>Descrizione: {{ $dress->description }}</li>
-                </ul>
+                <h1>Dettagli vestito con id: {{ $dress->id }}</h1>
+                <p>Nome: {{ $dress->type }}</p>
+                <p>Marca: {{ $dress->brand }}</p>
+                <p>Prezzo: {{ number_format($dress->price, 2, ',', ' ') }}</p>
+                <p>Colore: {{ $dress->color }}</p>
+                <p>Taglia: {{ $dress->size }}</p>
+                <p>Descrizione: {{ $dress->description }}</p>
             </div>
         </div>
     </div>
